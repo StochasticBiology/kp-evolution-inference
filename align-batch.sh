36d8d1af5a56947a5ddeac4c7c0ef38756f017da
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2024-10-21 10:45:12 +0200

    Option to do another dataset

--- align-batch.sh
+++ align-batch.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # ncbi_dataset contains NCBI reference Klebsiella genome
 # number 4... contains Zanzibar blood sample sequences
 
@@ -8,11 +10,17 @@
 REF_FILE="ncbi_dataset/ncbi_dataset/data/GCA_000240185.2/GCA_000240185.2_ASM24018v2_genomic.fna"
 
 # Define the base directory and the filename pattern
-BASE_DIR="./number4"
-#BASE_DIR="./tmp"
-OUT_DIR="${BASE_DIR}/outputs" 
-PATTERN="*.fna" 
+if [ "$1" == "1" ]; then
+  BASE_DIR="./number1"
+  PATTERN="*.fasta"
+fi
+if [ "$1" == "4" ]; then
+  BASE_DIR="./number4"
+  PATTERN="*.fna"
+fi
 
+# allocate and create output directory
+OUT_DIR="${BASE_DIR}/outputs" 
 mkdir "$OUT_DIR"
 
 # Loop through all files matching the pattern in the base directory and its subdirectories
