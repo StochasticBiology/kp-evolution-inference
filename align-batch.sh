6d6d8eb9bef62362c4c6a1ab925edc5da9e3af78
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2025-03-31 19:52:10 +0200

    Prune extraneous pipeline

--- align-batch.sh
+++ align-batch.sh
@@ -29,51 +29,10 @@ find "$BASE_DIR" -type f -name "$PATTERN" | while read -r file; do
     # Extract the filename without the directory path
     filename=$(basename "$file")
 
-    # Define the output file using the original filename (with .out extension, for example)
-    sam_output_file="${filename}_aligned.sam"
-    bam_output_file="${filename}_aligned.bam"
-    bam_output_sorted_file="${filename}_aligned_sorted.bam"
-    img_file="${filename}_alignment.png"
     report_file="${filename}.report"
 
     # Perform your desired operation on the file (e.g., process it)
     echo "Processing $file..."
-
-    # SAMs and BAMs don't play a role in the final version
-    #minimap2 -x asm5 -t 4 -a "$REF_FILE" "$file" > "$sam_output_file"
-    #samtools view -S -b "$sam_output_file" > "$bam_output_file"
-    #samtools sort "$bam_output_file" -o "$bam_output_sorted_file"
-    #samtools flagstat "$bam_output_sorted_file"
-
-    # scaffold with RagTag
-    ragtag.py scaffold "$REF_FILE" "$file" 
-
-    # just label RagTag output and pull into a consistent place
-    # Loop through all files in the subdirectory
-    for ragtagfile in "ragtag_output"/*; do
-	# Check if it's a file (skip directories)
-	if [ -f "$ragtagfile" ]; then
-            # Extract the directory and the filename
-            ragtagdirname=$(dirname "$ragtagfile")
-            ragtagfilename=$(basename "$ragtagfile")
-
-            # Create the new filename by appending the string
-            new_filename="${filename}-${ragtagfilename}"
-
-            # Rename the file
-            mv "$ragtagfile" "${OUT_DIR}/$new_filename"
-
-            echo "Renamed $ragtagfile to ${OUT_DIR}/$new_filename"
-	fi
-    done
-
-    # hard to visualise RagTag output? so visualise alignment from nucmer
-    nucmer --prefix=alignment "$REF_FILE" "$file"
-    mummerplot --png --layout --filter alignment.delta
-    # automated script production fails; use manual gnuplot call instead
-    #gnuplot "out.gp"
-    #cp "out.png" "${OUT_DIR}/${img_file}"
-
     # get report of alignment with respect to reference sequence
     echo "Running dnadiff"
     dnadiff "$REF_FILE" "$file"
