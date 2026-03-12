c81e18c298e7c20af1fc32c179abdf9a88ad38a5
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2026-03-12 12:26:07 +0100

    Restyle phylo vs non case study

--- kleborate-analysis/test-phylo.R
+++ kleborate-analysis/test-phylo.R
@@ -38,10 +38,24 @@ for(country in c("Romania", "Senegal")) {
     sf = 2
     fname = paste0(country, "-compare-both.png", collapse="")
     png(fname, width=900*sf, height=500*sf, res=72*sf)
-    ggarrange(plotHypercube.curated.tree(ct, hjust = 1, scale.fn = NULL, font.size = 2.5) + theme(
+    ggarrange(plotHypercube.curated.tree(ct[[country]], hjust = 1, scale.fn = NULL, font.size = 2.5) + theme(
       plot.margin = margin(t = 10, r = 10, b = 100, l = 10)) + coord_cartesian(clip = "off"),
       plotHypercube.bubbles.compare(parallelised.runs, sqrt.trans = TRUE),
       widths=c(1,2), labels=c("A", "B"))
+    dev.off()
+    
+    sf = 4
+    fname = paste0(country, "-compare-both-2.png", collapse="")
+    png(fname, width=400*sf, height=200*sf, res=72*sf)
+    ggarrange(
+      plotHypercube.curated.tree(ct[[country]], hjust = 1, scale.fn = NULL, font.size = 0) + 
+        #theme( plot.margin = margin(t = 10, r = 10, b = 100, l = 10)) + 
+        coord_cartesian(clip = "off"),
+      plotHypercube.bubbles.compare(parallelised.runs[c(1,4)], 
+                                    sqrt.trans = TRUE, 
+                                    expt.names = c("No phylo", "Phylo") ) +
+        theme(axis.ticks.y = element_blank(), axis.text.y  = element_blank()),
+      widths=c(1,2))
     dev.off()
     
     fname = paste0(country, "-data.Rdata", collapse="")
@@ -51,19 +65,4 @@ for(country in c("Romania", "Senegal")) {
 }
 
 #######
-
-load("Senegal-data.Rdata")
-p.1 = parallelised.runs
-load("Romania-data.Rdata")
-p.2 = parallelised.runs
-
-sf = 2
-fname = paste0(country, "-compare-both.png", collapse="")
-png(fname, width=900*sf, height=500*sf, res=72*sf)
-ggarrange(plotHypercube.curated.tree(ct, hjust = 1, scale.fn = NULL, font.size = 2.5) + theme(
-  plot.margin = margin(t = 10, r = 10, b = 100, l = 10)) + coord_cartesian(clip = "off"),
-  plotHypercube.bubbles.compare(parallelised.runs, sqrt.trans = TRUE),
-  widths=c(1,2), labels=c("A", "B"))
-dev.off()
-
 
